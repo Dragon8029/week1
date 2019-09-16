30f93c5378844951aa87dbb65cb93be98c483a08
Dragon8029 <dragon8029@gmail.com>
2019-09-15 19:27:56 -0700

    .

--- week-1/js/js.js
+++ week-1/js/js.js
@@ -1,6 +1,10 @@
 var table = document.getElementById("myTable");
 function load() {
-   
+   if (sales > 0) {
+       for (var i = 0; i < sales.length; i++) {
+           var row = table.insertRow(-1)
+       } 
+   }
 }
 
 
