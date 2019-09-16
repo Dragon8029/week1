4d6c05ebc11f7163bef50b02d875ddbcd7a4812c
Dragon8029 <dragon8029@gmail.com>
2019-09-15 18:34:11 -0700

    small changes

--- week-1/js/js.js
+++ week-1/js/js.js
@@ -1,6 +1,9 @@
-console.log("Javascript is connected");
+var table = document.getElementById("myTable");
 function load() {
     alert(sales[1]["Basic Cupcake:"])
     console.log(sales[0]);
 }
+
+
+
 
